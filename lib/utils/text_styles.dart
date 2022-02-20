6ec4e1a5260b0d1fd6200f897d15cff0f36013cc
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-20 21:25:31 +0530

    wip

---
+++ lib/utils/text_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomTextStyle {
+  static const TextStyle header = TextStyle(
+    fontSize: 16,
+    color: Colors.black,
+  );
+
+  static const TextStyle body = TextStyle(
+    fontSize: 16,
+    color: Colors.black,
+  );
+}

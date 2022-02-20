8614e1857dbc5053eeaa2e664160172ea66bda10
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-21 00:50:47 +0530

    colors. added

---
+++ lib/utils/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color deactive = Color(0xFF9696BB);
+  static const Color active = Color(0xFF5273E2);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFF9696BB);
+  static const Color black = Color(0xFF000000);
+  static const Color boxBackground = Color(0xFFF5F5FB);
+
+  static const Color inactiveDot = Color(0xFFD6D6E5);
+  static const Color background = Color(0xFFE5E5E5);
+  static const Color primaryBlue = Color(0xFF2574FB);
+  static const Color secondryBlue = Color(0xFF4C8DFB);
+}

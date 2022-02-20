81e6b9d98af40f559c92ec26d2acfeb53ff08ae8
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-21 00:50:53 +0530

    assets added

---
+++ lib/utils/assets.dart
@@ -0,0 +1,17 @@
+class Assets {
+  Assets._();
+
+  static const String filterIcon = 'assets/images/svg/filter.svg';
+  static const String moneyIcon = 'assets/images/svg/money.svg';
+  static const String moreIcon = 'assets/images/svg/more.svg';
+  static const String peoplesIcon = 'assets/images/svg/peoples.svg';
+  static const String walletIcon = 'assets/images/svg/wallet.svg';
+
+  //PNGS
+  static const String demo = 'assets/images/png/demo.png';
+  static const String demo1 = 'assets/images/png/demo1.png';
+
+  //circle
+  static const String eclipse1 = 'assets/images/svg/Ellipse1.svg';
+  static const String eclipse2 = 'assets/images/svg/Ellipse2.svg';
+}
